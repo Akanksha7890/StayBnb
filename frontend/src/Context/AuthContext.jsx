918c43e4b2e852ffde21972cc9966897129526d9
Akanksha7890 <akankshaupadhyay136@gmail.com>
2026-03-14 16:43:12 +0530

    Update AuthContext.jsx

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
     const serverUrl = 
-const serverUrl = "https://staybnb-backend-vwaj.onrender.com";
+const serverUrl = "https://staybnb-backend-vab5.onrender.com";
     let [loading,setLoading]=useState(false)
 
     let value={
