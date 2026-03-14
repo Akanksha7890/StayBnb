8b94bc457bc8d115fe78069c2787f1c8f202fec7
Akanksha7890 <akankshaupadhyay136@gmail.com>
2026-03-14 16:49:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", cors()); // allow preflight requests
+app.options("/*", cors()); // allow preflight requests
 
 // ✅ 2. Middlewares
 app.use(express.json({ limit: "50mb" }));
