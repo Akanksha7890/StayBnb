de94849da87a4e35f59896e80f3308fb23ed23d0
Akanksha7890 <akankshaupadhyay136@gmail.com>
2026-03-16 14:48:33 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,7 +14,7 @@ export const sighUp=async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT === "production",
+            secure:process.env.NODE_ENV === "production",
             sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
